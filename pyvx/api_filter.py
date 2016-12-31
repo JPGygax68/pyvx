afcaa3845b1d6329719594a553238df9bba98e4c
Jean-Pierre Gygax <gygax@practicomp.ch>
2016-12-31 18:29:08 +0100

    Making fine progress now with AST reassembly

--- pyvx/api_filter.py
+++ pyvx/api_filter.py
@@ -9,6 +9,8 @@ class APIFilter:
 
         self.index = Index.create()
 
+        self.tu = None # type: Cursor
+
         for filename in header_files:
             self.read_header(filename)
 
@@ -24,14 +26,35 @@ class APIFilter:
                 self.ppdefs[t.spelling] if t.spelling in self.ppdefs else t.spelling
                 for t in c.get_tokens() if t.kind != TokenKind.COMMENT]
 
-        def reassemble(c: Cursor):
-            return ' '.join(filtered_tokens(c))
+        def reassemble(cursor: Cursor):
+            #return ' '.join(filtered_tokens(c))
+            return ' '.join(c.spelling for c in cursor.get_children())
 
-        return '\n'.join([reassemble(c) for c in self.tu.cursor.get_children()
+        def node_to_string(cursor: Cursor) -> str:
+            if cursor.kind == CursorKind.ENUM_DECL:
+                defs = ["%s = %s" % (c.displayname, c.enum_value) for c in cursor.get_children()]
+                return 'enum %s { %s };' % (cursor.displayname, ', '.join(defs))
+            elif cursor.kind == CursorKind.TYPEDEF_DECL:
+                children = [_ for _ in cursor.get_children()]
+                if children and children[0].kind == CursorKind.STRUCT_DECL:
+                    mdefs = ['%s %s;' % (c.type.spelling, c.displayname) for c in children[0].get_children()]
+                    td = '%s {\n    ' % children[0].spelling + '\n    '.join(mdefs) + '\n}'
+                else:
+                    td = cursor.underlying_typedef_type.spelling
+                return 'typedef %s %s;' % (td, cursor.displayname)
+            else:
+                # TODO (the following is a placeholder)
+                return "/* %s: %s */" % (cursor.spelling, cursor.kind)
+
+        return '\n'.join([node_to_string(c) for c in self.tu.cursor.get_children()
                           if c.spelling[:2].lower() == "vx"])
 
     def dbg_print_all(self):
-        for c in self.tu.cursor.get_children():
-            if c.spelling[:2].lower() == 'vx':
-                s = ' '.join([t.spelling for t in c.get_tokens() if t.kind != TokenKind.COMMENT])
-                print(s)
+
+        def print_node(n: Cursor, indent):
+            print('  ' * indent + 'displayname: %s, kind: %s, type: %s' %
+                  (n.displayname, n.kind, n.result_type.spelling))
+            for c in n.get_children():
+                print_node(c, indent + 1)
+
+        print_node(self.tu.cursor, 0)
